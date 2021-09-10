7e333975b68b6a17c2ced7b53f21e96ff24ff08f
clebodam <clebodam@gmail.com>
2021-09-10 16:13:45 +0200

    remove unused import

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:herow_plugin_flutter/herow_plugin_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
